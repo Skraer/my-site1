7b945bb1d7f02763cb8e4d9ad750849076590d75
Skraer <kizilov.alex1823@gmail.com>
2019-11-16 01:02:55 +0300

    Добавление it_corp

---
+++ it_corp/main.js
@@ -0,0 +1,14 @@
+function ibg() {
+    let ibgElems = document.querySelectorAll('.ibg');
+    let imgArr = document.querySelectorAll('img');
+    
+    imgArr.forEach(function(img) {
+        ibgElems.forEach(function(div) {
+            if (div.contains(img)) {
+                let src = img.getAttribute('src');
+                div.style.backgroundImage = 'url(' + src + ')';
+            }
+        });
+    });
+}
+ibg();
