ec1b82af70185f2d8981ec0763ceaa8b628cdcc8
Skraer <kizilov.alex1823@gmail.com>
2020-06-19 16:10:41 +0300

    pools

---
+++ pools/js/main.js
@@ -0,0 +1,24 @@
+// function ibg() {
+//     let ibgElems = document.querySelectorAll('.ibg');
+//     let imgArr = document.querySelectorAll('img');
+    
+//     imgArr.forEach(function(img) {
+//         ibgElems.forEach(function(div) {
+//             if (div.contains(img)) {
+//                 let src = img.getAttribute('src');
+//                 div.style.backgroundImage = 'url(' + src + ')';
+//             }
+//         });
+//     });
+// }
+// ibg();
+
+// // ПЛАВНЫЙ ПЕРЕХОД К РАЗДЕЛАМ ЛЕНДИНГА ЧЕРЕЗ МЕНЮ
+// let menuItems = document.querySelectorAll(CSS_КЛАССЫ_ПУНКТОВ_МЕНЮ);
+// menuItems.forEach(function(elem) {
+//     elem.addEventListener('click', function() {
+//         let href = elem.getAttribute('href').substr(1);
+//         event.preventDefault();
+//         document.getElementById(`${href}`).scrollIntoView({block: "start", behavior: "smooth"});
+//     });
+// });
