385e61893b9dfb05764fba0d370faa7a3f418bae
Александр Кизилов <kizilov.alex1823@gmail.com>
2021-02-26 09:11:46 +0300

    ...

---
+++ test/oto/index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>Володин Мебель - Кухни на заказ в Тамбове</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="/oto/css/style.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+    <header>
+        <div class="fulcontent flex">
+            <div class="iteim logo">
+                <img src="/oto/img/logo.png" alt=""> 
+                <p class="cubtaitl">Кухни на заказ от производителя</br>  в Тамбове и области</p>
+            </div>
+            <div class="iteim info">
+                <a class="viber" href="viber://chat?number=79161543201">
+                    <img src="/oto/img/Viber.png" alt="">
+                </a>
+                <a class="whatsapp" href="https://wa.me/79161543201">
+                    <img src="/oto/img/Whatsapp.png" alt="">
+                </a>
+                <div class="hpone">
+                    <a href="tel:">8 (916) 154-32-01</a>
+                    <span>На связи 9:00 - 21:00</span>
+                </div>
+            </div>
+        </div>
+    </header>
+<div class="content">
+    <p class="taitl">Спасибо за заявку!</br> У нас есть для вас выгодное предложение!</p>
+    <div class="taitl_info">Закажите замер и дизайн-проект кухни прямо сейчас и</div>
+    <div class="animate_block">
+        <span></span>
+        <b>сэкономьте 800 рублей</b>
+    </div>
+</div>
+    <div class="form_block">
+        <div class="content">
+            <div class="form_content">
+                <div class="uznat">
+                    <div class="">
+                    <span>
+                        <img src="/oto/img/info.png" alt="">
+                        Узнать что входит
+                    </span>
+                    </div>
+                </div>
+                <div class="fxodit">
+                    <div class="border">
+                        <img class="close" src="/oto/img/close.png" alt="">
+						<p>Наш дизайнер:</p>
+                        <ul>
+                            <li>1. Сделает замер вашего помещения</li>
+                            <li>2. Проведет аудит помещения</li>
+                            <li>3. Совместно с вами спроектирует кухню вашей мечты</li>
+                            <li>4. Подберет фурнитуру, которая будет максимально вам удобна</li>
+                            <li>5. Предложит вам материалы, которые удовлетворят вашим эстетическим потребностям</li>
+                            <li>6. Поможет подобрать плитку на фартук, пол, стены.</li>
+                            <li>7. Даст чертежи плитки и розеток</li>
+                            <li>8. Даст рекомендации по расположению и скрытию коммуникаций (систем водоснабжения, газоснабжения, вентиляции)</li>
+                            <li>9. Даст рекомендации по установке гипсокартонного короба над кухней</li>
+                            <li>10. Посоветует как избежать ошибок при заказе кухни и сэкономить на этом до 100 000 рублей</li>
+                            <li>11. 3D визуализацию вашей будущей кухни</li>
+                            <li>12. 3 расчёта стоимости: премиум, средней+ и стандарт+</li>
+                        </ul>
+                    </div> 
+                </div>
+                <p class="uznatafter">
+                    <span>
+                    Предложение действует один раз при оплате </br>
+                        в течение 15 минут. Успейте заказать замер с выгодой!
+                    </span>
+                </p>
+                <div class="paimerblock">
+                    <div class="border">
+                         <span>
+                            До конца нашего </br>
+                            супер-выгодного предложения:
+                        </span>
+                        <div id="taimer"></div>
+                        <form  id="form" method="POST" onsubmit="ym(64740358, 'reachGoal', 'zayavka'); gtag('event', 'Заявка', {'event_category': 'zayavka'}); fbq('track', 'Lead');">
+                            <div class="label phone" >
+                                <input type="text"  placeholder="Введите телефон" name="userPhone" id="phone">
+                            </div>
+                            <div class="submit_row">
+                                <div class="submit">
+                                    <input type="submit" value="Заказать замер за 699 руб">
+                                </div>
+                                <span class="oldprais">1499</span>
+                            </div>
+                            <span class="sog">Нажимая на кнопку вы соглашаетесь с нашей <a href="Политика конф..pdf" target="_blank">политикой конфиденциальности</a></span>
+                        </form>
+                    </div>
+                </div>
+                <p class="suminfo">Эта сумма вычитается из стоимости изделия  при оформлении договора.</p>
+            </div>
+        </div>
+    </div>
+<footer>
+    <div class="fulcontent flex">
+        <div class="iteim info">
+            <div class="hpone">
+             <a href="tel:">8 (916) 154-32-01</a>
+                 <span>На связи 9:00 - 21:00</span>
+            </div>
+        </div>
+        <div class="iteim logo">
+            <img src="/oto/img/logofooter.png" alt="">
+        </div>
+        <div class="iteim">
+            <div class="social">
+                <a href="https://www.facebook.com/mebel.volodin/"><img src="/oto/img/facebook.png" alt=""></a>
+                <a href="https://vk.com/volodin.mebel"><img src="/oto/img/social.png" alt=""></a>
+                <a href="https://www.instagram.com/volodin.mebel/"><img src="/oto/img/instagram.png" alt=""></a>
+                <a href="https://ok.ru/volodin.mebel"><img src="/oto/img/odnoklassniki.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</footer>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="/oto/js/jquery.plugin.min.js"></script>
+    <script type="text/javascript" src="/oto/js/jquery.countdown.min.js"></script>
+    <script type="text/javascript" src="/oto/js/jquery.countdown-ru.js"></script>
+    <script type="text/javascript" src="/oto/js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="/oto/js/main.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.4-beta.33/jquery.inputmask.min.js" ></script>
+	<div class="tranks">
+		<div class="content">
+			<img class="close" src="/oto/img/close.png" alt="">
+			<p>Спасибо что заказали замер в нашей компании</p>
+		</div>
+	</div>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(62483122, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/62483122" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160738462-6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-160738462-6');
+</script>
+
+ <!-- Facebook Pixel Code -->
+      <script>
+      	!function(f,b,e,v,n,t,s)
+      	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      	n.queue=[];t=b.createElement(e);t.async=!0;
+      	t.src=v;s=b.getElementsByTagName(e)[0];
+      	s.parentNode.insertBefore(t,s)}(window, document,'script',
+      	'https://connect.facebook.net/en_US/fbevents.js');
+      	fbq('init', '248054713036076');
+      	fbq('track', 'PageView');
+		fbq('track', 'Lead');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      	src="https://www.facebook.com/tr?id=248054713036076&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
+	
+	
+</body>
+</html>
---
+++ test/oto/thy/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <title>Володин Мебель - Спасибо</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="/oto/css/style.css" type="text/css" rel="stylesheet" />
+
+</head>
+
+<body>
+<div class="content" style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);">
+    <p class="taitl">Спасибо за заявку!</p>
+</div>
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="oto/js/jquery.plugin.min.js"></script>
+    <script type="text/javascript" src="oto/js/jquery.countdown.min.js"></script>
+    <script type="text/javascript" src="oto/js/jquery.countdown-ru.js"></script>
+    <script type="text/javascript" src="oto/js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="oto/js/main.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.4-beta.33/jquery.inputmask.min.js" ></script>
+	<div class="tranks">
+		<div class="content">
+			<img class="close" src="/oto/img/close.png" alt="">
+			<p>Спасибо что заказали замер в нашей компании</p>
+		</div>
+	</div>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(62483122, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/62483122" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160738462-6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-160738462-6');
+</script>
+
+ <!-- Facebook Pixel Code -->
+      <script>
+      	!function(f,b,e,v,n,t,s)
+      	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      	n.queue=[];t=b.createElement(e);t.async=!0;
+      	t.src=v;s=b.getElementsByTagName(e)[0];
+      	s.parentNode.insertBefore(t,s)}(window, document,'script',
+      	'https://connect.facebook.net/en_US/fbevents.js');
+      	fbq('init', '248054713036076');
+      	fbq('track', 'PageView');
+		fbq('track', 'Tripwire');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      	src="https://www.facebook.com/tr?id=248054713036076&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
+	
+	
+</body>
+</html>
