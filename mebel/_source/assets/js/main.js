5ca067292f180a9b8dd29b34a3c3a010f7816019
Александр Кизилов <kizilov.alex1823@gmail.com>
2021-03-16 02:08:39 +0300

    belbarrier added

--- mebel/_source/assets/js/main.js
+++ mebel/_source/assets/js/main.js
@@ -34,3 +34,101 @@ inputTel.addEventListener('mouseleave', function () {
         this.value = '';
     }
 });
+
+
+function sendForm(form, onSuccess) {
+    onSuccess = onSuccess || function(){};
+    event.preventDefault();
+    if (validateForm(form)) {
+        var xhr = new XMLHttpRequest();
+        var body = serialize(form);
+        xhr.open('POST', './mail.php');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                form.reset();
+                // showSuccessWindow();
+                onSuccess();
+                alert('Данные отправлены');
+                // setTimeout(function() {
+                //     window.location.href = '/thanks.html';
+                // }, 1500);
+            }
+        };
+        xhr.send(body);
+    } else {
+        alert('Введите корректные данные');
+        // form.reset();
+    }
+}
+function validateForm(form) {
+	var regTel = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+	var inputTel = form.querySelector('input[name="tel"]');
+    var inputName = form.querySelector('input[name="name"]');
+    if (inputTel.value.match(regTel) && inputName.value.length > 2) return true;
+    else return false;
+}
+function serialize(form) {
+	if (!form || form.nodeName !== "FORM") {
+		return false;
+	}
+	var i, j, q = [];
+	for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+		if (form.elements[i].name === "") {
+			continue;
+		}
+		switch (form.elements[i].nodeName) {
+			case 'INPUT':
+				switch (form.elements[i].type) {
+					case 'text':
+					case 'tel':
+					case 'email':
+					case 'hidden':
+					case 'password':
+					case 'button':
+					case 'reset':
+					case 'submit':
+						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						break;
+					case 'checkbox':
+						writeCommaCheckbox(form, i, q);
+						break;
+					case 'radio':
+						if (form.elements[i].checked) {
+							q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						}
+						break;
+				}
+				break;
+			case 'file':
+				break;
+			case 'TEXTAREA':
+				q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+				break;
+			case 'SELECT':
+				switch (form.elements[i].type) {
+					case 'select-one':
+						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						break;
+					case 'select-multiple':
+						for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+							if (form.elements[i].options[j].selected) {
+								q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+							}
+						}
+						break;
+				}
+				break;
+			case 'BUTTON':
+				switch (form.elements[i].type) {
+					case 'reset':
+					case 'submit':
+					case 'button':
+						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						break;
+				}
+				break;
+		}
+	}
+	return q.join("&");
+}
