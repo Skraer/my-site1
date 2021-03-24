29ffcd8fa8ae37292905c7843fd410a7306eb186
Александр Кизилов <kizilov.alex1823@gmail.com>
2021-03-25 00:43:37 +0300

    ...

--- metall/_source/assets/js/main.js
+++ metall/_source/assets/js/main.js
@@ -74,6 +74,171 @@ class ProductItemCart {
     }
 }
 
+
+class Modal {
+    constructor(selector, {onCall}) {
+        this.selector = selector;
+        this.el = document.querySelector(this.selector);
+        if (this.el) {
+            this.callBtn = document.querySelector('[data-call="' + this.el.getAttribute('id') + '"]');
+            this.onCall = onCall || function(){};
+            this.overlay = this.el.querySelector('.modal__overlay');
+            this.closeBtn = this.el.querySelector('.modal__close');
+            this.setup();
+        }
+    }
+    showModal(disableAnimation = false) {
+        // activeModal = this;
+        if (disableAnimation) {
+            this.el.classList.add('active');
+        } else {
+            this.el.classList.remove('hiding');
+            this.el.classList.add('active');
+            this.el.classList.add('showing');
+            setTimeout(() => {
+                this.el.classList.remove('showing');
+            }, 250);
+        }
+        document.body.classList.add('lock');
+        this.onCall();
+    }
+    hideModal(disableAnimation = false) {
+        // activeModal = null;
+        if (disableAnimation) {
+            this.el.classList.remove('active');
+        } else {
+            this.el.classList.remove('showing');
+            this.el.classList.add('hiding');
+            setTimeout(() => {
+                this.el.classList.remove('active');
+                this.el.classList.remove('hiding');
+            }, 250);
+        }
+        document.body.classList.remove('lock');
+    }
+    showingHandler() {
+        this.el.classList.contains('active') ?
+            this.hideModal() :
+            this.showModal();
+    }
+    setup() {
+        this.closeBtn.addEventListener('click', (e) => {
+            this.hideModal();
+        });
+        this.overlay.addEventListener('mousedown', (e) => {
+            if (e.target === this.overlay) {
+                this.hideModal();
+            }
+        });
+        this.callBtn.addEventListener('click', e => {
+            this.showModal();
+        });
+    }
+}
+
+const callMe = new Modal('#callMe', {});
+const productPhoto = new Modal('#productPhoto', {});
+
+function sendForm(form, onSuccess = null) {
+    onSuccess = onSuccess || function(){};
+    event.preventDefault();
+    if (validateForm(form)) {
+        var xhr = new XMLHttpRequest();
+        var body = serialize(form);
+        console.log(body);
+        xhr.open('POST', './mail.php');
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                form.reset();
+                onSuccess();
+                form.setAttribute('data-done', null);
+                if (form.hasAttribute('data-after')) {
+                    activeModal ? activeModal.hideModal() : void(0);
+                    const id = form.getAttribute('data-after');
+                    afterModals[id].showModal();
+                }
+            }
+        };
+        xhr.send(body);
+    } else {
+        alert('Введите корректные данные');
+    }
+}
+function validateForm(form) {
+	var regTel = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+	var inputTel = form.querySelector('input[name="tel"]');
+    var inputName = form.querySelector('input[name="name"]');
+    var allow = true;
+    if (inputTel && !inputTel.value.match(regTel)) allow = false;
+    if (inputName && inputName.value.length < 2) allow = false;
+    return allow;
+}
+function serialize(form) {
+	if (!form || form.nodeName !== "FORM") {
+		return false;
+	}
+	var i, j, q = [];
+	for (i = form.elements.length - 1; i >= 0; i = i - 1) {
+		if (form.elements[i].name === "") {
+			continue;
+		}
+		switch (form.elements[i].nodeName) {
+			case 'INPUT':
+				switch (form.elements[i].type) {
+					case 'text':
+					case 'tel':
+					case 'email':
+					case 'hidden':
+					case 'password':
+					case 'button':
+					case 'reset':
+					case 'submit':
+						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						break;
+					case 'checkbox':
+						writeCommaCheckbox(form, i, q);
+						break;
+					case 'radio':
+						if (form.elements[i].checked) {
+							q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						}
+						break;
+				}
+				break;
+			case 'file':
+				break;
+			case 'TEXTAREA':
+				q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+				break;
+			case 'SELECT':
+				switch (form.elements[i].type) {
+					case 'select-one':
+						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						break;
+					case 'select-multiple':
+						for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+							if (form.elements[i].options[j].selected) {
+								q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
+							}
+						}
+						break;
+				}
+				break;
+			case 'BUTTON':
+				switch (form.elements[i].type) {
+					case 'reset':
+					case 'submit':
+					case 'button':
+						q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
+						break;
+				}
+				break;
+		}
+	}
+	return q.join("&");
+}
+
 if (document.querySelectorAll('.product-item__cart')) {
     const productCarts = document.querySelectorAll('.product-item__cart');
     productCarts.forEach(cart => {
