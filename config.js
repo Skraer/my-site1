374cc72f9815d79d044fbba7944dcc5b2b7d6227
Alexander Kizilov <kizilov.alex1823@gmail.com>
2021-08-06 16:42:23 +0300

    ...

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 window.process = {
   env: {
+    PUSH_SERVER_KEY:
+      'BBWMd-Y-6jg5dEbkcDfXRPBfLGGgEy6GZOlHz7N9Xt-GSi8jfRTBDMIy3x73H3huOEN_APhbznUK9SL4kusA6X8',
     API: 'https://api.jsonbin.io/v3/b/610ba3b2f098011544ab9c36',
     API_KEY: '$2b$10$jg/vdrqjqXai8GGpUvKii.Q3DvClvOa3KapnRq6DlC2FgE7wUQKIW',
     HEADERS: {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 5
+const version = 6
 const staticCacheName = `static-booking-v${version}`
 const dynamicCacheName = `dynamic-booking-v${version}`
 
